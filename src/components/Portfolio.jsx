4b219c307bf267ee4a1b6ece10854300fe8dc8bc
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2026-02-01 06:15:42 +0500

    updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Portfolio.css';
 import PortfolioImg1 from '../assets/images/vyral-media.png';
 import PortfolioImg2 from '../assets/images/home-without-login.png';
-import PortfolioImg3 from '../assets/images/vyral-media.png';
+import PortfolioImg3 from '../assets/images/portfolio.png';
 
 const projects = [
   {
@@ -16,19 +16,19 @@ const projects = [
   {
     title: 'Darzify',
     description: 'A location-based platform for finding nearby tailors, boutiques, and designers with easy order and measurement management.',
-    tech: 'React, OpenCV.js, JavaScript',
+    tech: 'HTML, Tailwind CSS, JavaScript',
     image: PortfolioImg2,
     liveLink: 'https://darzify.netlify.app/',
     githubLink: 'https://github.com/AbdulAziz2397/darzify.git'
   },
-  // {
-  //   title: 'Portfolio Website',
-  //   description: 'Personal portfolio to showcase projects and skills.',
-  //   tech: 'React, CSS, Firebase Hosting',
-  //   image: PortfolioImg3,
-  //   liveLink: 'https://abdulaziz-tahir.netlify.app',
-  //   githubLink: 'https://github.com/yourusername/portfolio'
-  // }
+  {
+    title: 'Portfolio Website',
+    description: 'A clean and responsive portfolio website showcasing my skills, projects, and services as a MERN Stack Developer. Built with modern web technologies and focused on performance and usability.',
+    tech: 'React, Tailwind CSS, JavaScript',
+    image: PortfolioImg3,
+    liveLink: 'https://abdulaziz-tahir.netlify.app',
+    githubLink: 'https://github.com/AbdulAziz2397/AbdulAziz-Tahir-.git'
+  }
 ];
 
 const Portfolio = () => {
